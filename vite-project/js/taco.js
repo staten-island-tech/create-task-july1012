ad2cbcd6205089c53a35a9999b11161cb88ef44e
july1012 <113192987+july1012@users.noreply.github.com>
2024-01-12 13:14:47 -0500

    um

--- vite-project/js/taco.js
+++ vite-project/js/taco.js
@@ -1,6 +1,9 @@
 taco [
     {
-        shell: ["hardshell", "softshell"],
-        protein: ["steak", "chicken", "pork", "fish"]
+        shell: ["hardshell", "concha dura","softshell", "tortilla"],
+        protein: ["steak", "chicken", "pork", "fish", "beans", "turkey","shrimp","pollo", "carne asada","al pastor", "birria", "barabcoa","camarón","pez", "frijoles"],
+        vegetables: ["lettuce", "carrots", "avocado", "garlic","cucumber","cilantro","pickled jalapeño","red onion", "zucchini", "mushroom", "squash", "potatoes", "red peppers", "green peppers", "tomatoes","cauliflower","bell peppers"],
+        rice:["white", "black", "brown", "cilantro lime"],
+        sauces:["Salsa de Chamoy", "Pico de Gallo","Salsa Verde","Guacamole-Style Salsa Verde"," Salsa Roja","Salsa de Chile de Árbol","Salsa Tamulada","Guajillo Chile Salsa", "Sour Cream", "Guacamole", "Hot Sauce"],
     }
 ]
