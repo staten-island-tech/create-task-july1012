e0ffd06e74f4fe85024a310e2d37ea1de57d5724
july1012 <113192987+july1012@users.noreply.github.com>
2024-01-17 13:15:28 -0500

    sdsd

--- vite-project/js/taco.js
+++ vite-project/js/taco.js
@@ -1,5 +1,51 @@
 taco [
     {
+        type: "shell",
+        food1:{ name: "hardshell",
+        spanish: "concha dura",
+        myprice: "$0.50",
+        wholefoodURL: "https://www.wholefoodsmarket.com/product/365-by-whole-foods-market-organic-yellow-taco-shells-12-taco-shells-b074h577w3"
+    },
+        food2:{
+            name: "softshell",
+            spanish: "tortilla",
+            myprice: "$0.50",
+            wholefoodURL: "https://www.wholefoodsmarket.com/product/vista-hermosa-flour-tortillas-8ct-b07drkbgjw"
+        }
+    },
+    {
+        type: "protein",
+        food1:{
+        name: "steak",
+        spanish: "carne asada",
+        myprice: "$13",
+        wholefoodURL:"https://www.wholefoodsmarket.com/product/eat-meati-meati-carne-asada-steak-b0c4g25114",
+    },
+        food2: {name: "chicken",
+        spanish: "pollo",
+        myprice: "$11",
+        wholefoodURL:"https://www.wholefoodsmarket.com/product/whole-foods-market-organic-chicken-breasts-b08538nwft",
+    },
+        food3:{name: "pork",
+        spanish: "al pastor",
+        myprice: "$8",
+        wholefoodURL:"https://www.wholefoodsmarket.com/product/applegate-natural-ground-pork-16oz-b08dk6hygg",
+    },
+        food4:{name: "fish (tilapia)",
+        spanish: "pez",
+        myprice: "$15",
+        wholefoodURL:"https://www.wholefoodsmarket.com/product/365-by-whole-foods-market-tilapia-fillet-b07nrdrlrl",
+    },
+        food5:{ name: "beans",
+        spanish: "frijoles",
+        myprice: "$",
+        wholefoodURL:"",
+    },
+},
+
+  
+    
+    {
         shell: ["hardshell", "concha dura","softshell", "tortilla"],
         protein: ["steak", "chicken", "pork", "fish", "beans", "turkey","shrimp","pollo", "carne asada","al pastor", "birria", "barabcoa","camarón","pez", "frijoles"],
         vegetables: ["lettuce", "carrots", "avocado", "garlic","cucumber","cilantro","pickled jalapeño","red onion", "zucchini", "mushroom", "squash", "potatoes", "red peppers", "green peppers", "tomatoes","cauliflower","bell peppers"],
@@ -7,3 +53,9 @@ taco [
         sauces:["Salsa de Chamoy", "Pico de Gallo","Salsa Verde","Guacamole-Style Salsa Verde"," Salsa Roja","Salsa de Chile de Árbol","Salsa Tamulada","Guajillo Chile Salsa", "Sour Cream", "Guacamole", "Hot Sauce"],
     }
 ]
+
+{ name: "softshell",
+    spanish: "tortilla",
+    myprice: "$0.50",
+    wholefoodURL:"",
+},
