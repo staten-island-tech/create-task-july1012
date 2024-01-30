7b083c2c4be91837ac7353baf883652493ad9f7e
july1012 <113192987+july1012@users.noreply.github.com>
2024-01-30 13:18:52 -0500

    sfrgbs

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,1 +1,15 @@
 import "../css/style.css";
+
+const url = "http://taco-randomizer.herokuapp.com/";
+async function data(url) {
+  try {
+    const response = await fetch(url);
+    const dataa = await response.json();
+    insert(dataa);
+    console.log("Data:", dataa[0]);
+    getData(dataa);
+  } catch (error) {
+    console.log(error);
+  }
+}
+data(url);
